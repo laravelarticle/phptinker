e37146c674b02773642d0e45a5053c84ec7ed23f
MD.HARUN-UR-RASHID <harun.cox@gmail.com>
2023-01-02 10:50:44 +0600

    site

---
+++ script/app.js
@@ -0,0 +1,29 @@
+var myApp = angular.module('myApp', []);
+myApp.controller('AppCtrl', function ($scope) {
+
+  $scope.getOS = getOS
+
+  function getOS() {
+    var userAgent = window.navigator.userAgent,
+      platform = window.navigator ?.userAgentData ?.platform || window.navigator.platform,
+      macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+      windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+      iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+      os = null;
+
+    if (macosPlatforms.indexOf(platform) !== -1) {
+      os = 'Mac OS';
+    } else if (iosPlatforms.indexOf(platform) !== -1) {
+      os = 'iOS';
+    } else if (windowsPlatforms.indexOf(platform) !== -1) {
+      os = 'Windows';
+    } else if (/Android/.test(userAgent)) {
+      os = 'Android';
+    } else if (/Linux/.test(platform)) {
+      os = 'Linux';
+    }
+
+    return os;
+  }
+
+});
